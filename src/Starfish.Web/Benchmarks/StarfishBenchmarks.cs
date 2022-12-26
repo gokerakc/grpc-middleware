13822c3085877c3d55eca3fead6a66ec3a5495b9
goker <gokerakce8@gmail.com>
2022-12-26 20:41:24 +0000

    Added BenchmarkDotNet lib

---
+++ src/Starfish.Web/Benchmarks/StarfishBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Starfish.Web.Benchmarks;
+
+[MemoryDiagnoser]
+public class StarfishBenchmarks
+{
+    [Params(100)]
+    public int Amount { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+    
+    
+    [Benchmark]
+    public async Task DoSomething()
+    {
+       // Do Something
+    }
+}
