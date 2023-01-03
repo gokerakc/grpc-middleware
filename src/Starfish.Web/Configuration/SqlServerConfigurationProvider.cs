2c1e21d64097ed6d1509b47805acda19b2098720
goker <gokerakce8@gmail.com>
2023-01-03 22:57:01 +0000

    Refactored custom configuration provider

--- src/Starfish.Web/Configuration/SqlServerConfigurationProvider.cs
+++ src/Starfish.Web/Configuration/SqlServerConfigurationProvider.cs
@@ -1,28 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Starfish.Infrastructure.Data;
 using Starfish.Shared;
-using Starfish.Web.Extensions;
-using Starfish.Web.Options;
 
 namespace Starfish.Web.Configuration;
 
 public class SqlServerConfigurationProvider : ConfigurationProvider, IDisposable
 {
-    public SqlServerConfigurationSource Source { get; }
-
+    private SqlServerConfigurationSource Source { get; }
     private readonly Timer? _timer;
 
-    private readonly ILogger<SqlServerConfigurationProvider> _logger;
-    
     public SqlServerConfigurationProvider(SqlServerConfigurationSource source)
     {
         Source = source;
 
-        _logger = source.LoggerFactory.CreateLogger<SqlServerConfigurationProvider>();
-
         if (Source.ReloadPeriodically)
         {
-            _timer = new Timer(ReloadSettings, null, TimeSpan.Zero, TimeSpan.FromSeconds(Source.PeriodInSeconds));
+            _timer = new Timer(ReloadSettings, null, TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(Source.PeriodInSeconds));
         }
     }
 
@@ -30,39 +23,21 @@ public class SqlServerConfigurationProvider : ConfigurationProvider, IDisposable
     {
         var builder = new DbContextOptionsBuilder<DataContext>();
         Source.OptionsAction(builder);
-
         using (var dbContext = new DataContext(builder.Options))
         {
-            if (!dbContext.Database.CanConnect())
+            try
             {
-                _logger.DatabaseConnectionError($"{nameof(SqlServerConfigurationProvider)} can not connect to the database.");
-                return;
+                dbContext.Database.EnsureCreated();
+
+                Data = dbContext.StarfishSettings.ToDictionary<StarfishSettings, string, string?>(c => c.Id, c => c.Value, StringComparer.OrdinalIgnoreCase);
             }
-            
-            Data = dbContext.StarfishSettings.Any()
-                ? dbContext.StarfishSettings.ToDictionary<StarfishSettings, string, string?>(c => c.Id, c => c.Value, StringComparer.OrdinalIgnoreCase)
-                : CreateAndSaveDefaultValues(dbContext);
+            catch
+            {
+                // Ignore
+            }
         }
     }
 
-    private static IDictionary<string, string?> CreateAndSaveDefaultValues(
-        DataContext context)
-    {
-        var settings = new Dictionary<string, string?>(
-            StringComparer.OrdinalIgnoreCase)
-        {
-            [$"{nameof(StarfishLoggingOptions)}:{nameof(StarfishLoggingOptions.RequestLoggingEnabled)}"] = "false"
-        };
-
-        context.StarfishSettings.AddRange(
-            settings.Select(kvp => new StarfishSettings(kvp.Key, kvp.Value!))
-                .ToArray());
-
-        context.SaveChanges();
-
-        return settings;
-    }
-    
     private void ReloadSettings(object? state)
     {
         Load();
