fe587af6d4a11e40164baa3ca5e46338ae15872a
goker <gokerakce8@gmail.com>
2023-01-09 22:06:40 +0000

    Added unit tests for dto mappings

---
+++ test/Starfish.Tests/Mappings/BankAccountDtoTests.cs
@@ -0,0 +1,45 @@
+﻿using Starfish.Core.Models;
+using Starfish.Infrastructure.DTOs;
+
+namespace Starfish.Tests.Mappings;
+
+public class BankAccountDtoTests
+{
+    [Fact]
+    public void ShouldMapToDtoAsExpected()
+    {
+        var bankAccount = new BankAccount
+        {
+            Id = Guid.NewGuid(),
+            Balance = 550.00m,
+            AccountName = "Account Name",
+            AccountNumber = "11552244"
+        };
+
+        var dto = BankAccountDto.FromBankAccount(bankAccount);
+        
+        Assert.Equal(bankAccount.Id, dto.Id);
+        Assert.Equal(bankAccount.Balance, dto.Balance);
+        Assert.Equal(bankAccount.AccountName, dto.AccountName);
+        Assert.Equal(bankAccount.AccountNumber, dto.AccountNumber);
+    }
+    
+    [Fact]
+    public void ShouldMapToBankAccountAsExpected()
+    {
+        var dto = new BankAccountDto
+        {
+            Id = Guid.NewGuid(),
+            Balance = 550.00m,
+            AccountName = "Account Name",
+            AccountNumber = "11552244"
+        };
+
+        var bankAccount = (BankAccount)dto;
+        
+        Assert.Equal(dto.Id, bankAccount.Id);
+        Assert.Equal(dto.Balance, bankAccount.Balance);
+        Assert.Equal(dto.AccountName, bankAccount.AccountName);
+        Assert.Equal(dto.AccountNumber, bankAccount.AccountNumber);
+    }
+}
---
+++ test/Starfish.Tests/Mappings/BankTransactionDtoTests.cs
@@ -0,0 +1,53 @@
+﻿using Starfish.Core.Models;
+using Starfish.Infrastructure.DTOs;
+
+namespace Starfish.Tests.Mappings;
+
+public class BankTransactionDtoTests
+{
+    [Fact]
+    public void ShouldMapToDtoAsExpected()
+    {
+        var bankTransaction = new BankTransaction
+        {
+            Id = Guid.NewGuid(),
+            SourceId = Guid.NewGuid(),
+            TargetId = Guid.NewGuid(),
+            Amount = 550.00m,
+            Description = "Desc",
+            Date = DateTime.UtcNow
+        };
+
+        var dto = BankTransactionDto.FromBankTransaction(bankTransaction);
+        
+        Assert.Equal(bankTransaction.Id, dto.Id);
+        Assert.Equal(bankTransaction.SourceId, dto.SourceId);
+        Assert.Equal(bankTransaction.TargetId, dto.TargetId);
+        Assert.Equal(bankTransaction.Amount, dto.Amount);
+        Assert.Equal(bankTransaction.Description, dto.Description);
+        Assert.Equal(bankTransaction.Date, dto.Date);
+    }
+    
+    [Fact]
+    public void ShouldMapToBankTransactionAsExpected()
+    {
+        var dto = new BankTransactionDto
+        {
+            Id = Guid.NewGuid(),
+            SourceId = Guid.NewGuid(),
+            TargetId = Guid.NewGuid(),
+            Amount = 550.00m,
+            Description = "Desc",
+            Date = DateTime.UtcNow
+        };
+
+        var bankTransaction = (BankTransaction)dto;
+        
+        Assert.Equal(dto.Id, bankTransaction.Id);
+        Assert.Equal(dto.SourceId, bankTransaction.SourceId);
+        Assert.Equal(dto.TargetId, bankTransaction.TargetId);
+        Assert.Equal(dto.Amount, bankTransaction.Amount);
+        Assert.Equal(dto.Description, bankTransaction.Description);
+        Assert.Equal(dto.Date, bankTransaction.Date);
+    }
+}
